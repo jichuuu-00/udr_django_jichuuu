605b2914bab00b484a096ccab740ccdc2db5b599
DESKTOP-FGE3A8G\cry04 <jichuuu0417@gmail.com>
2022-01-30 16:04:37 +0900

    django course 41

--- articleapp/models.py
+++ articleapp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=False)
